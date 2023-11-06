46c24575e1cddbad7d5e80fdb219366f67a2c12e
Constantin Vizir <vizirconstantin.89@gmail.com>
2023-11-06 22:41:32 +0200

    first commit

---
+++ ConsoleKeys/Program.cs
@@ -0,0 +1,33 @@
+﻿string _val = "";
+Console.Write("Enter your value: ");
+ConsoleKeyInfo key;
+
+do
+{
+    key = Console.ReadKey(true);
+    if (key.Key != ConsoleKey.Backspace)
+    {
+        double val = 0;
+        string chars = "0123456789+-*/()";
+        //bool _x = double.TryParse(key.KeyChar.ToString(), out val);
+        bool _x = chars.Contains(key.KeyChar);
+        if (_x)
+        {
+            _val += key.KeyChar;
+            Console.Write(key.KeyChar);
+        }
+    }
+    else
+    {
+        if (key.Key == ConsoleKey.Backspace && _val.Length > 0)
+        {
+            _val = _val.Substring(0, (_val.Length - 1));
+            Console.Write("\b \b");
+        }
+    }
+}
+// Stops Receving Keys Once Enter is Pressed
+while (key.Key != ConsoleKey.Enter);
+
+Console.WriteLine();
+Console.WriteLine("The Value You entered is : " + _val);
